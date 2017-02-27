abebbca735e1d5dacf243af6ec7d1adf5f136a7a
aitorgarciavall <aitor.g.v.info@gmail.com>
2017-02-27 17:09:31 +0100

    Yeld arreglat

--- client/main.js
+++ client/main.js
@@ -1,5 +1,2 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-import '../imports/ui/components/cos/cos.js';
-import '../imports/ui/layouts/baseLayout.js';
+
 import '../imports/startup/client/routes.js';
